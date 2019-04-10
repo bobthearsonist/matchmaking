aa1fae3d16ca7a694fb7f80e6ae872a8d07b5a50
mperry <martinperry@gatech.edu>
2019-04-10 17:26:56 -0400

    added model files from ef generate

---
+++ PlayerMatcher/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerMatcherService.Models
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            GameSessions = new HashSet<GameSessions>();
+            Rating = new HashSet<Rating>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public bool? IsOnline { get; set; }
+
+        public virtual ICollection<GameSessions> GameSessions { get; set; }
+        public virtual ICollection<Rating> Rating { get; set; }
+    }
+}
