3a1ea510a5c78ed8aac330fe296016b7ea047974
Chris Taylor <mastrixlord@gmail.com>
2019-04-21 10:24:59 -0400

    Added the skeleton of a matchmaker class file

---
+++ PlayerMatcher/Matchmaker/Matchmaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlayerMatcher.Matchmaker
+{
+    public class PlayerData
+    {
+        private int playerRating;
+        private int userID;
+        private List<int> behaviorAttributes;
+
+        public PlayerData(int id, int rating)
+        {
+            this.userID = id;
+            this.playerRating = rating;
+            this.behaviorAttributes = new List<int>();
+        }
+        public PlayerData(int id, int rating, List<int> attributes)
+        {
+            this.userID = id;
+            this.playerRating = rating;
+            this.behaviorAttributes = attributes;
+        }
+
+    }
+
+    public class Matchmaker
+    {
+        private PlayerMatcherEntities db = new PlayerMatcherEntities();
+
+        public List<PlayerData> ConstructMatch(int gameID, int numPlayers)
+        {
+            int numTaken = 0; 
+
+            return null;
+        }
+    }
+
+    
+}
