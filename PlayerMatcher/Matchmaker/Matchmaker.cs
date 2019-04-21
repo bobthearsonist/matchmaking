cbbdcf0b23f168e91cfd1bed0e0f3c7be2d7a0a1
Chris Taylor <mastrixlord@gmail.com>
2019-04-21 14:46:12 -0400

    First iteration of ConstructMatch algorithm finalized

--- PlayerMatcher/Matchmaker/Matchmaker.cs
+++ PlayerMatcher/Matchmaker/Matchmaker.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace PlayerMatcher.Matchmaker
 {
-    public class PlayerData
+    /*public class PlayerData
     {
         private int playerRating;
         private int userID;
@@ -24,37 +24,71 @@ namespace PlayerMatcher.Matchmaker
             this.behaviorAttributes = attributes;
         }
 
-    }
+    }*/
 
     public class Matchmaker
     {
-        private PlayerMatcherEntities db = new PlayerMatcherEntities();
-        
-
-        public List<PlayerData> ConstructMatch(int gameID, int numPlayers)
+        public Matchmaker()
         {
-            int numTaken = 0;
 
-            int minElo = 0;
-            int maxElo = 10000;
-
-            var GetPlayersQuery =
-                from players in db.Users
-                join ranks in db.Ratings on ranks.User_ID equals players.User_ID
-                where ranks.Game_ID == gameID && ranks.User_Rating > minElo && ranks.User_Rating < maxElo
-                orderby players.User_Name
-                select new
-                {
-                    PlayerName = players.User_Name,
-                    EloRating = ranks.User_Rating
-                };
-
-
-
-
-            return null;
         }
-    }
 
-    
+        private PlayerMatcherEntities db = new PlayerMatcherEntities();
+        public List<User> ConstructMatch(int gameID, int numPlayers)
+        {
+            try
+            {
+                int numTaken = 0;
+                int numToTake = 1;
+                int minElo = 0;
+                int maxElo = 10000;
+                List<User> playersInMatch = new List<User>();
+                List<int> listOfElos = new List<int>();
+
+                while (numTaken != numPlayers)
+                {
+                    if (numTaken != 0)
+                    {
+                        numToTake = numPlayers - numTaken;
+                    }
+                    var GetPlayersQuery =
+                        from players in db.Users
+                        join ranks in db.Ratings on players.User_ID equals ranks.User_ID
+                        where ranks.Game_ID == gameID && ranks.User_Rating > minElo && ranks.User_Rating < maxElo
+                        orderby ranks.User_Rating ascending
+                        select new
+                        {
+                            Player = players,
+                            EloRating = ranks.User_Rating.GetValueOrDefault()
+                        };
+                    foreach (var matchPlayer in GetPlayersQuery)
+                    {
+                        playersInMatch.Add(matchPlayer.Player);
+                        listOfElos.Add(matchPlayer.EloRating);
+                        numTaken++;
+                        if (playersInMatch.Count == numToTake)
+                        {
+                            break;
+                        }
+                    }
+                    if (numTaken == 1)
+                    {
+                        minElo = listOfElos[0] - 12;
+                        maxElo = listOfElos[0] + 12;
+                    }
+                    else
+                    {
+                        minElo = minElo - 12;
+                        maxElo = maxElo - 12;
+                    }
+                }
+                return playersInMatch;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught exception. Suspect that Elo rating was missing");
+                return null;
+            }
+        }
+    }    
 }
