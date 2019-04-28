cf1462c2a5fb3a2bc6dc7f5dbcf9e8db2a0f0f37
mperry <martinperry@gatech.edu>
2019-04-27 20:14:07 -0400

    more tests to cover error cases

---
+++ PlayerMatcher/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using PlayerMatcher.Models;
+using System.Web.Mvc;
+
+namespace PlayerMatcher.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected PlayerMatcherEntitiesExtended db;
+
+        public BaseController() : this(new PlayerMatcherEntitiesExtended()) { }
+
+        public BaseController(PlayerMatcherEntities db)
+        {
+            this.@db = @db as PlayerMatcherEntitiesExtended;
+        }
+    }
+}
